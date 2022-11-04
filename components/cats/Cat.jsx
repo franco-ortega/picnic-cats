9b1fcb29beb12486c0fde5ce315fd07b40fb6b06
Franco Ortega <francopdx@gmail.com>
2022-11-03 17:29:00 -0700

    configure Next to use images from TheCatAPI

--- components/cats/Cat.jsx
+++ components/cats/Cat.jsx
@@ -4,8 +4,7 @@ import React from 'react';
 const Cat = ({ url }) => {
   return (
     <button>
-      <Image src={url} alt='' />
-      Meow
+      <Image src={url} alt='' width='200' height={200} />
     </button>
   );
 };
--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -1,10 +1,15 @@
 import { useCats } from '../../hooks/useCats';
+import CatList from '../cats/CatList';
 
 const Home = () => {
   const { cats } = useCats();
 
   console.log(cats);
-  return <div>Home</div>;
+  return (
+    <div>
+      <CatList cats={cats} />
+    </div>
+  );
 };
 
 export default Home;
