87172ebb04f39bbc1d39799b644e17dbedcb0052
Franco Ortega <francopdx@gmail.com>
2022-11-04 20:59:46 -0700

    create Loading component that slides down

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -2,14 +2,17 @@ import { useCats } from '../../hooks/useCats';
 import { usePicnicCats } from '../../hooks/usePicnicCats';
 import CatList from '../cats/CatList';
 import PicnicCatList from '../cats/PicnicCatList';
+import Loading from '../loading/Loading';
 
 const Home = () => {
-  const { cats } = useCats();
+  const { cats, loading } = useCats();
   const { picnicCats, addPicnicCat, picnicCatCount } = usePicnicCats();
 
   return (
     <div>
-      {picnicCatCount < 3 ? (
+      {loading ? (
+        <Loading />
+      ) : picnicCatCount < 3 ? (
         <CatList
           cats={cats}
           addPicnicCat={addPicnicCat}
--- hooks/useCats.js
+++ hooks/useCats.js
@@ -3,12 +3,16 @@ import { getCats } from '../services/cats';
 
 export const useCats = () => {
   const [cats, setCats] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (cats.length === 0) {
-      getCats().then((res) => setCats(res));
+      getCats().then((res) => {
+        setCats(res);
+        setLoading(false);
+      });
     }
   }, [cats]);
 
-  return { cats };
+  return { cats, loading };
 };
